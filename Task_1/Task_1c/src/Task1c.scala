99be9394f0cb57cfe276e8c232d8c7eaae2f801d
lelouvincx <dinhminhchinh3357@gmail.com>
2024-05-25 18:09:46 +0700

    Solve Task_1c

--- Task_1/Task_1c/src/Task1c.scala
+++ Task_1/Task_1c/src/Task1c.scala
@@ -3,16 +3,33 @@ import org.apache.spark.sql.types._
 import org.apache.spark.SparkContext
 
 object Main {
+  def groupBalance(balance: Int): String = {
+    if (balance <= 500) { // low if from -inf to 500
+      "low"
+    } else if (balance <= 1500) { // medium if from 501 to 1500
+      "medium"
+    } else { // high if from 1501 to +inf
+      "high"
+    }
+  }
+
   def solution(sc: SparkContext) {
     // Load each line of the input data
-    val bankdataLines = sc.textFile("Assignment_Data/bank-small.csv")
+    val bankdataLines = sc.textFile("Assignment_Data/bank.csv")
     // Split each line of the input data into an array of strings
     val bankdata = bankdataLines.map(_.split(";"))
 
     // TODO: *** Put your solution here ***
+    val groupByBalance = bankdata.map(row => {
+      val balance = row(5).toString.toInt
+      val group = groupBalance(balance)
+      (group, 1)
+    })
 
+    val sumByBalance = groupByBalance.reduceByKey(_ + _)
 
-
+    // Save to text file
+    sumByBalance.coalesce(1, shuffle = true).saveAsTextFile("Task_1c-out")
   }
 
   // Do not edit the main function
